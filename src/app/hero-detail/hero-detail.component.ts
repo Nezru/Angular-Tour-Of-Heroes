c9ba8229e3bf6cbbc0ea9301b407441229e0f4bf
Chris <chris.rollings@lineview.com>
2020-12-01 11:41:22 +0000

    Use hero detail componenet

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -6,7 +6,7 @@ import { Hero } from '../heroes/hero';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-@Input() hero: Hero;
+  @Input() hero: Hero;
   constructor() { }
 
   ngOnInit(): void {
